ce79d8c1e835a9ba94697a1136da54a7b1d277ab
Phoebe Lau <phoebelau7@gmail.com>
2018-01-24 23:53:00 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,6 @@ class PostsController < ApplicationController
 
   def create
     Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
   end
 end
